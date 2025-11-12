f5ffeedd7e8294b95bb3810b695b83899ea10098
m <mananmulchandani@gmail.com>
2025-11-13 01:34:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000,
-    outDir: 'dist',
-  },
-  base: '/', // Change this from './' to '/'
 })
